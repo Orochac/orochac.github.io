8e89ec65df2a00afa9e49c118afa564a16df90c0
Orochac <O.Rochanakij@protonmail.com>
2024-08-03 20:31:32 +0700

    rm cname file

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -16,4 +16,13 @@ var store = [{
         "tags": [],
         "url": "/Week-two/",
         "teaser": null
-      }]
+      },{
+        "title": "Week three",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/Week-three/",
+        "teaser": null
+      },{
+    "title": "About",
+    "excerpt":"This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll...","url": "http://localhost:4000/about/"
+  }]
